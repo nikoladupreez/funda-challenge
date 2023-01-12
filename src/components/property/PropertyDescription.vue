b43b5fc9327b77a738a77ec5804ba533d486616e
Nikola du Preez <nikola_dupreez@hotmail.com>
2023-01-12 04:56:26 +0100

    description

---
+++ src/components/property/PropertyDescription.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="property-description">
+    <ShowMoreContent title="Omschrijving" height="200px">
+        <p>{{ description }}</p>
+    </ShowMoreContent>
+  </div>
+</template>
+
+<script>
+import ShowMoreContent from '../ShowMoreContent';
+
+export default {
+    name: 'PropertyDescription',
+    components: {
+        ShowMoreContent
+    },
+    props: {
+        description: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.property-description {
+    padding: 24px 0;
+    border-bottom: 1px solid lightgrey;
+
+    p {
+        white-space: pre-line;
+    }
+}
+</style>
