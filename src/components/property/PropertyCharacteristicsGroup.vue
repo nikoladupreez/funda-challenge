ed5c4d8202a36e75826815392f205acc08557046
Nikola du Preez <nikola_dupreez@hotmail.com>
2023-01-12 04:56:32 +0100

    characteristics

---
+++ src/components/property/PropertyCharacteristicsGroup.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="property-characteristics-group">
+        <h3>{{ title }}</h3>
+        <div 
+            v-for="(item, key) in items"
+            :key="key"
+            class="property-characteristics-group__item"
+        >
+            <h4>{{item.Naam}}</h4>
+            <div v-html="item.Waarde"/>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PropertyCharacteristicsGroup',
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        items: {
+            type: Array,
+            default: () => []
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.property-characteristics-group {
+    margin-bottom: 20px;
+
+    &__item {
+        padding: 10px 0;
+        border-top: 1px solid rgb(235, 233, 233);
+
+        @media (min-width: #{$m}) {
+            display: flex;
+
+            h4 {
+                width: 300px;
+            }
+        }
+    }
+}
+</style>
---
+++ src/components/property/PropertyCharacterstics.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="property-characteristics">
+    <ShowMoreContent title="Kenmerken" height="250px">
+        <PropertyCharacteristicsGroup
+            v-for="(characteristic, key) in characteristics"
+            :key="key"
+            :title="characteristic.Titel"
+            :items="characteristic.Kenmerken"
+        />
+    </ShowMoreContent>
+  </div>
+</template>
+
+<script>
+import ShowMoreContent from '../ShowMoreContent.vue';
+import PropertyCharacteristicsGroup from './PropertyCharacteristicsGroup.vue';
+
+export default {
+    name: "PropertyCharacteristics",
+    components: {
+        ShowMoreContent,
+        PropertyCharacteristicsGroup
+    },
+    props: {
+        characteristics: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.property-characteristics {
+    padding: 24px 0;
+}
+</style>
